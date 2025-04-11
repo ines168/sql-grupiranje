d798bf890814dec926a07c13c503d50ae840cdf6
Ines <ines.cvek16@gmail.com>
2025-04-11 22:14:02 +0200

    ...

--- DZ.sql
+++ DZ.sql
@@ -53,3 +53,149 @@ SELECT p.Naziv, SUM(s.Kolicina) AS ProdanihPrimjeraka, SUM(s.UkupnaCijena) AS Za
 FROM Proizvod AS p INNER JOIN Stavka AS s ON p.IDProizvod=s.ProizvodID 
 GROUP BY p.Naziv
 HAVING SUM(s.Kolicina)>2000 AND SUM(s.UkupnaCijena)>2000000
+
+
+--1. Ispišite sve potkategorije i za svaku ispišite broj proizvoda u njoj.
+SELECT Naziv,
+	(SELECT COUNT(*) AS BrojProizvoda FROM Proizvod WHERE IDPotkategorija=PotkategorijaID)
+FROM Potkategorija
+
+--2. Riješite prethodni zadatak pomoću spajanja.
+SELECT pk.Naziv, COUNT(*) AS BrojProizvoda 
+FROM Potkategorija AS pk INNER JOIN Proizvod AS p ON pk.IDPotkategorija=p.PotkategorijaID
+GROUP BY pk.Naziv;
+
+--3. Ispišite sve kategorije i za svaku ispišite broj proizvoda u njoj.
+SELECT k.Naziv AS Kategorija, 
+	(SELECT COUNT(*) FROM Proizvod AS p WHERE p.PotkategorijaID IN 
+		(SELECT pk.IDPotkategorija FROM Potkategorija AS pk WHERE pk.IDPotkategorija=k.IDKategorija)
+	) AS BrojProizvoda
+FROM Kategorija AS k
+
+--4. Ispišite sve proizvode i pokraj svakog ispišite zarađeni iznos, od najboljih prema lošijim.
+SELECT Naziv, 
+	(SELECT SUM(s.UkupnaCijena) FROM Stavka AS s WHERE IDProizvod=s.ProizvodID) AS Zarada
+FROM Proizvod
+ORDER BY Zarada DESC
+
+--5. Dohvatite sve proizvode, uz svaki proizvod ispišite prosječnu cijenu svih proizvoda 
+--te razliku prosječne cijene svih proizvoda i cijene tog proizvoda. U obzir uzmite samo proizvode s cijenom većom od nule.
+SELECT Naziv, CijenaBezPDV,
+	(SELECT AVG(CijenaBezPDV) FROM Proizvod WHERE CijenaBezPDV > 0) AS Prosjek,
+	CijenaBezPDV-(SELECT AVG(CijenaBezPDV) FROM Proizvod WHERE CijenaBezPDV > 0) AS Razlika
+FROM Proizvod
+WHERE CijenaBezPDV > 0
+
+--6. Dohvatite imena i prezimena 5 komercijalista koji su izdali najviše računa.
+SELECT TOP (5) Ime, Prezime, 
+	(SELECT COUNT(*)  FROM Racun WHERE KomercijalistID=IDKomercijalist) AS BrojRacuna
+FROM Komercijalist
+ORDER BY BrojRacuna DESC
+
+--7. Dohvatite imena i prezimena 5 najboljih komercijalista po broju realiziranih računa te uz svakog dohvatite i iznos prodane robe.
+SELECT TOP (5) Ime, Prezime, 
+	(SELECT COUNT(*) FROM Racun WHERE KomercijalistID=IDKomercijalist) AS BrojRacuna,
+	(SELECT SUM(UkupnaCijena) FROM Stavka AS s WHERE s.RacunID IN 
+		(SELECT r.IDRacun FROM Racun AS r WHERE r.KomercijalistID=IDKomercijalist)
+	) AS ProdaniIznos
+FROM Komercijalist
+ORDER BY BrojRacuna DESC
+
+--8. Dohvatite sve boje proizvoda. Uz svaku boju pomoću podupita dohvatite broj proizvoda u toj boji.
+SELECT DISTINCT p1.Boja ,
+	(SELECT COUNT(*) FROM Proizvod AS p2 WHERE p1.Boja=p2.Boja) AS BrojProizvoda
+FROM Proizvod AS p1
+WHERE p1.Boja IS NOT NULL
+
+--9. Dohvatite imena i prezimena svih kupaca iz Frankfurta i uz svakog ispišite broj računa koje je platio karticom, od višeg prema nižem.
+SELECT Ime, Prezime,
+	(SELECT COUNT(*) FROM Racun AS r WHERE IDKupac=r.KupacID AND r.KreditnaKarticaID IS NOT NULL) AS BrojRacuna
+FROM Kupac
+WHERE GradID=
+	(SELECT IDGrad FROM Grad WHERE Naziv='Frankfurt')
+ORDER BY BrojRacuna DESC
+
+--10. Vratite sve proizvode čija je cijena pet ili više puta veća od prosjeka.
+SELECT * FROM Proizvod AS p
+WHERE p.CijenaBezPDV>
+	5*(SELECT AVG(CijenaBezPDV) FROM Proizvod)
+
+--11. Vratite sve proizvode koji su prodavani, ali u količini manjoj od 5.
+SELECT Naziv
+FROM Proizvod
+WHERE IDProizvod IN
+	(SELECT ProizvodID FROM Stavka WHERE Kolicina<5)
+
+SELECT p.Naziv
+FROM Proizvod AS p
+WHERE p.IDProizvod IN
+	(SELECT s.ProizvodID FROM Stavka AS s WHERE 
+		(SELECT SUM(s2.Kolicina) FROM Stavka AS s2 WHERE s2.ProizvodID=p.IDProizvod)<5
+	)
+
+SELECT 
+    p.Naziv
+FROM Proizvod p
+WHERE (
+    SELECT SUM(s.Kolicina)
+    FROM Stavka s
+    WHERE s.ProizvodID = p.IDProizvod
+) < 5
+AND EXISTS (
+    SELECT 1
+    FROM Stavka s
+    WHERE s.ProizvodID = p.IDProizvod
+);
+--12. Vratite sve proizvode koji nikad nisu prodani:
+--* Pomoću IN ili NOT IN
+--* Pomoću EXISTS ili NOT EXISTS
+--* Pomoću spajanja
+SELECT * 
+FROM Proizvod 
+WHERE IDProizvod NOT IN 
+	(SELECT ProizvodID FROM Stavka )
+
+SELECT * 
+FROM Proizvod
+WHERE NOT EXISTS
+  (SELECT * FROM Stavka AS s WHERE s.ProizvodID=IDProizvod)
+
+SELECT * FROM Proizvod AS p LEFT JOIN Stavka AS s ON p.IDProizvod = s.ProizvodID
+WHERE s.ProizvodID IS NULL
+
+--13. Vratite količinu zarađenog novca za svaku boju proizvoda.
+--NE ZNAM
+SELECT p.Boja,
+	(SELECT SUM(s.UkupnaCijena) FROM Stavka AS s WHERE s.ProizvodID
+	) AS Zarada
+FROM Proizvod AS p
+
+--JOIN
+SELECT p.Boja,
+       SUM(s.UkupnaCijena) AS Zarada
+FROM Proizvod p
+JOIN Stavka s ON p.IDProizvod = s.ProizvodID
+GROUP BY p.Boja;
+
+
+--14. Vratite količinu zarađenog novca za svaku boju proizvoda, ali samo za one boje koje su zaradile više od 20.000.000.
+--NE ZNAM
+
+--JOIN
+SELECT p.Boja,
+       SUM(s.UkupnaCijena) AS Zarada
+FROM Proizvod p
+JOIN Stavka s ON p.IDProizvod = s.ProizvodID
+GROUP BY p.Boja
+HAVING SUM(s.UkupnaCijena)>20000000
+
+
+--15. Vratiti sve proizvode koji imaju dodijeljenu potkategoriju i koji su prodani u količini većoj od 5000. Uz svaki proizvod vratiti prodanu količinu i naziv kategorije.
+SELECT Naziv, 
+	(SELECT SUM(s.Kolicina) FROM Stavka AS s WHERE s.ProizvodID=IDProizvod) AS ProdanaKolicina,
+	(SELECT k.Naziv FROM Kategorija AS k WHERE k.IDKategorija=
+		(SELECT KategorijaID FROM Potkategorija WHERE IDPotkategorija=PotkategorijaID)
+	) AS Kategorija
+FROM Proizvod
+WHERE PotkategorijaID IS NOT NULL AND 
+	(SELECT SUM(s.Kolicina) FROM Stavka AS s WHERE s.ProizvodID=IDProizvod)>5000
